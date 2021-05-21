7d5bcead43a8136bcf2289c15bd19bddb236895f
Ahmed <ahmedchouihi@gmail.com>
2021-05-21 12:17:36 +0100

    'addTables'

---
+++ laravelbackend/app/Http/Controllers/TodayenergyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todayenergy;
+use Illuminate\Http\Request;
+
+class TodayenergyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Todayenergy  $todayenergy
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todayenergy $todayenergy)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Todayenergy  $todayenergy
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Todayenergy $todayenergy)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Todayenergy  $todayenergy
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todayenergy $todayenergy)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todayenergy  $todayenergy
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todayenergy $todayenergy)
+    {
+        //
+    }
+}
